98ea0e7ee77a800d15ffb322b07611c68b066e81
paramireze <paramireze@gmail.com>
2017-11-23 14:37:11 -0600

    Added footer and header

---
+++ application/views/templates/header.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Results</title>
+        <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/bootstrap.min.css">
+    </head>
+    <body>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <a class="navbar-brand"></a>
+        </div>
+    </nav>
