3620839d25ed3143b92c6d60e60981cda012551e
paramireze <paramireze@gmail.com>
2017-11-28 21:15:23 -0600

    Added db connection and display posts

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -5,6 +5,8 @@ class Posts extends CI_Controller {
 
         $data['title'] = "latest posts";
 
+        $data['posts'] = $this->post_model->get_posts();
+
         $this->load->view('templates/header');
         $this->load->view('posts/index', $data);
         $this->load->view('templates/footer');
--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,3 +1,7 @@
 
 <h1><?= $title ?></h1>
-<p>this is index php page</p>
+<?php foreach($posts as $post) : ?>
+<h3><?php echo $post['title']; ?></h3>
+<small class="post-date">Posted on: <?php echo $post['created_at']; ?></small><br />
+<?php echo $post['body']; ?>
+<?php endforeach; ?>
