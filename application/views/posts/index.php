c0f786e24608343ad5cf266e786e1d4b2728ffc6
paramireze <paramireze@gmail.com>
2017-11-29 20:42:03 -0600

    Display posts and individual posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,7 +1,8 @@
 
 <h1><?= $title ?></h1>
 <?php foreach($posts as $post) : ?>
-<h3><?php echo $post['title']; ?></h3>
-<small class="post-date">Posted on: <?php echo $post['created_at']; ?></small><br />
-<?php echo $post['body']; ?>
+    <h3><?php echo $post['title']; ?></h3>
+    <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small><br />
+    <?php echo $post['body']; ?>
+    <p><a class="btn btn-primary" href="<?php echo site_url('/posts/'.$post['slug']); ?>">read more</a></p>
 <?php endforeach; ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,36 +1,30 @@
 <html>
     <head>
         <title>Results</title>
-        <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
         <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/style.css">
-<!--        <link ref="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">-->
+
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Results</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url(); ?>/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url(); ?>/posts">Blog</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                add login here?
-<!--                <input class="form-control mr-sm-2" type="text" placeholder="Search">-->
-<!--                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>-->
-            </form>
+    <div class="container">
+        <hr />
+        <div class="header clearfix">
+            <nav>
+                <ul class="nav nav-pills float-right">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
+                    </li>
+                </ul>
+            </nav>
+            <h3 class="text-muted">Madison Hash Race Results Page</h3>
         </div>
-    </nav>
-    <div class="container"><br /><br />
+        <hr />
 
