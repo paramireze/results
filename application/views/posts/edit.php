ef11c6536e59239e24fa11f44c5602ab92c54b5e
paramireze <paramireze@gmail.com>
2017-12-23 14:20:20 -0600

    Added dropdown menu for categories

--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -12,6 +12,23 @@
     <label for="exampleInputPassword1">body</label>
     <textarea class="form-control" name="body" placeholder="add body" ><?php echo $post['body']; ?></textarea>
 </div>
+<div class="form-group">
+    <label>Category</label>
 
+    <select name="category_id" class="form-control">
+        <?php foreach($categories as $category): ?>
+            <option <?php if ($category['id'] == $post['category_id']) { echo 'selected'; } else { echo 'nope';}  ?> value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?> </option>
+        <?php endforeach; ?>
+    </select>
+</div>
 <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+<?php
+echo '<pre>';
+print_r($post);
+echo '</pre>';
+echo '<pre>';
+print_r($categories);
+echo '</pre>';
+?>
