c1cddc63ee72c9aefd0dda3d42362e985b9ca24c
paramireze <paramireze@gmail.com>
2018-01-06 15:06:44 -0600

    Added jquery data tables

--- application/views/people/index.php
+++ application/views/people/index.php
@@ -1,11 +1,29 @@
 
 <h1><?= $title ?></h1>
 <hr />
-<?php foreach($people as $person) : ?>
-    <h3><?php echo $person['p_id'] . ': ' . $person['p_first_name'] . ' ' .  $person['p_last_name']; ?></h3>
-    <div>Email: <?php echo $person['p_email']; ?></div><br />
-    <div>Phone: <?php echo $person['p_phone']; ?></div><br />
-    <div>Address: <?php echo $person['p_address']; ?></div><br />
-    <div>DOB: <?php echo $person['p_dob']; ?></div><br />
-    <hr />
-<?php endforeach; ?>
+<table id="dataTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Gender</th>
+    </tr>
+    </thead>
+    <?php foreach($people as $person) : ?>
+        <tr>
+            <td><?php echo $person['p_id'] . ': ' . $person['p_first_name'] . ' ' .  $person['p_last_name']; ?></td>
+            <td><?php echo $person['p_gender']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+
+    <tfoot>
+    <tr>
+        <th>Name</th>
+        <th>Gender</th>
+    </tr>
+    </tfoot>
+    <tbody>
+
+
+
+    </tbody>
+</table>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,10 +1,19 @@
 <html>
     <head>
         <title>Results</title>
+        <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/style.css">
+        <script type = "text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+        <script type = "text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+        <script type = "text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.2.1/dt-1.10.16/r-2.2.1/datatables.min.css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-        <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/style.css">
-
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.2.1/dt-1.10.16/r-2.2.1/datatables.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#dataTable').DataTable();
+            } );
+        </script>
     </head>
     <body>
 
@@ -19,6 +28,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo base_url(); ?>races">Races</a>
                     </li>
+
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo base_url(); ?>people">People</a>
                     </li>
