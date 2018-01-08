b63ef0b167c40718d24e91bcaee2fd2a71f76f7c
paramireze <paramireze@gmail.com>
2018-01-07 19:19:47 -0600

    wrap content in content container for styling purposes

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,32 +1,62 @@
-<h2><?= $title ?></h2>
-<p>welcome to the results application</p>
-<hr />
-<h2>goal</h2>
-<p>create an application that tracks race results for Hash Races Events </p>
-<hr />
-<h2>Under Construction</h2>
-<p>I am not sure if or when this application will ever be complete. This is a fun project that I work on when I have time. </p>
-    <hr />
-<h2>version 1 features</h2>
-<ul>
-    <li>list all madidsonh3 races or other races we care about</li>
-    <li>display individual results for each race we care about</li>
-    <li>report on race participants current and past race results</li>
-</ul>
-<hr />
-<h2>change log</h2>
-<ul>
-    <li>Jan 1  - create db diagram and display on home page
-    <li>Dec 31 - create database query to look up race using parameters passed in URL</li>
-    <li>Dec 29 - make URL's look pretty by using page slugs</li>
-    <li>Dec 28 - create base pages for displaying race events and people</li>
-    <li>Dec 26 - create database tables for tracking race events</li>
-    <li>Dec 10 - build out race pages</li>
-</ul>
-<hr />
-<div>
-    code commits can be found on my git hub page <a href='https://github.com/paramireze/results'>https://github.com/paramireze/results</a>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="display-1"><?= $title ?></h1>
+            </div>
+        </div>
+    </div>
 </div>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="">Welcome to the results application</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>goal</h2>
+                <p>create an application that tracks race results for Hash Races Events </p>
+                <hr />
+                <h2>Under Construction</h2>
+                <p>I am not sure if or when this application will ever be complete. This is a fun project that I work on when I have time. </p>
+                <hr />
+                <h2>version 1 features</h2>
+                <ul>
+                    <li>list all madidsonh3 races or other races we care about</li>
+                    <li>display individual results for each race we care about</li>
+                    <li>report on race participants current and past race results</li>
+                </ul>
+                <hr />
+                <h2>change log</h2>
+
+                Jan 06 - display races, people and events in a table that can be sorted with search bar.<br />
+                Jan 06 - improve database structure so it handles a racers gender, age and division place<br />
+                Jan 01 - created race_participants table and added base page for view. added db diagram for home page<br />
+                Dec 31 - create database query to look up race using parameters passed in URL<br />
+                Dec 29 - make URL's look pretty by using page slugs<br />
+                Dec 28 - create base pages for displaying race events and people<br />
+                Dec 26 - create database tables for tracking race events<br />
+                Dec 10 - build out race pages<br />
+
+                <hr />
+                <div>
+                    code commits can be found on my git hub page <a href='https://github.com/paramireze/results'>https://github.com/paramireze/results</a>
+                </div>
+                <hr />
+                <h2>Db Diagram</h2>
+                <image src="https://snag.gy/2NLxpR.jpg"  width="500px;"/>
+            </div>
+        </div>
+    </div>
+</div>
+<h2></h2>
+<p></p>
 <hr />
-<h2>Db Diagram</h2>
-<image src="https://snag.gy/2NLxpR.jpg"  width="500px;"/>
+
