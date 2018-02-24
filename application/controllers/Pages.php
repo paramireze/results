da80cb20ace87c7dc936c92926b860096f5cd768
paramireze <paramireze@gmail.com>
2018-02-23 19:23:36 -0600

    Display all races on front page

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,7 +6,16 @@
                 show_404();
             }
 
+            $this->load->model('race_type_model');
+            $this->load->model('race_model');
+            $this->load->model('race_participant_model');
+            $race_types = $this->race_type_model->get_race_types();
+
+            $data['race_types'] = $race_types;
+
+
             $data['title'] = ucfirst($page);
+
             $this->load->template('pages/'.$page, $data);
 
         }
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,56 +1,26 @@
-
-<div class="py-5">
+<div class="py-5 text-center bg-light">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="display-1"><?= $title ?></h1>
+                <h1>Come Run With Us</h1>
+                <p class="lead">We grow together with the community</p>
             </div>
         </div>
-    </div>
-</div>
-
-<div class="py-5">
-    <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <h1 class="">Welcome to the results application</h1>
-                <h2>goal</h2>
-                <p>create an application that tracks race results for Hash Races Events </p>
-                <hr />
-                <h2>Under Construction</h2>
-                <p>I am not sure if or when this application will ever be complete. This is a fun project that I work on when I have time. </p>
-                <hr />
-                <h2>version 1 features</h2>
-                <ul>
-                    <li>list all madidsonh3 races or other races we care about</li>
-                    <li>display individual results for each race we care about</li>
-                    <li>report on race participants current and past race results</li>
-                </ul>
-                <hr />
-                <h2>change log</h2>
-                Jan 17 - didn't do much other than drink beer at brocach
-                Jan 16 - played with images and how things display on race page
-                Jan 07 - added header and footer styling
-                Jan 06 - display races, people and events in a table that can be sorted with search bar.<br />
-                Jan 06 - improve database structure so it handles a racers gender, age and division place<br />
-                Jan 01 - created race_participants table and added base page for view. added db diagram for home page<br />
-                Dec 31 - create database query to look up race using parameters passed in URL<br />
-                Dec 29 - make URL's look pretty by using page slugs<br />
-                Dec 28 - create base pages for displaying race events and people<br />
-                Dec 26 - create database tables for tracking race events<br />
-                Dec 10 - build out race pages<br />
+        <?php
 
-                <hr />
-                <div>
-                    code commits can be found on my git hub page <a href='https://github.com/paramireze/results'>https://github.com/paramireze/results</a>
-                </div>
-                <hr />
-                <h2>Db Diagram</h2>
-                <image src="https://snag.gy/2NLxpR.jpg"  width="500px;"/>
-            </div>
+
+        foreach ($race_types as $race_type) { ?>
+            <div class="col-md-4 p-4">
+                <p><b><?php echo $race_type['rt_name']; ?></b></p>
+                <img class="img-fluid d-block rounded-circle mx-auto" src="<?php echo $race_type['rt_image_url']; ?>">
+                <p class="my-4"><i><?php echo $race_type['rt_description']; ?></i></p>
+
+            </div><?php
+
+        }
+
+        ?>
         </div>
     </div>
 </div>
-
-<hr />
-
