d4f43ce943196e7c9c208c5695cc8caca1a567a5
paramireze <paramireze@gmail.com>
2018-02-26 07:24:22 -0600

    Added title

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="utf-8">
+    <title>Results</title>
+    <link rel="icon" href="<?= asset_url();?>images/favicon-beer.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
     <link rel="stylesheet" href="https://v40.pingendo.com/assets/bootstrap/bootstrap-4.0.0-beta.1.css" type="text/css"> </head>
