66aa7260ec445026223cb0074d936ab988411e05
paramireze <paramireze@gmail.com>
2018-02-26 07:21:06 -0600

    Html fixes

--- application/controllers/Races.php
+++ application/controllers/Races.php
@@ -26,9 +26,6 @@ class Races extends CI_Controller {
                 foreach ($participantsTopMales as $participantTopMale) {
                     $participantsTopMalesArray[$participantTopMale['p_slug']] = array('p_first_name' => $participantTopMale['p_first_name'], 'p_last_name' => $participantTopMale['p_last_name'],'rp_age' => $participantTopMale['rp_age'], 'rp_time' => $participantTopMale['rp_time']);
                 }
-                echo '<pre>';
-                print_r($participantsTopMales);
-                echo '</pre>';
 
                 /* get top three female participants for each race */
                 $participantsTopFemales = $this->race_participant_model->get_participants_top_finishers($race['race_id'], 3, "F");
@@ -74,7 +71,7 @@ class Races extends CI_Controller {
             foreach ($participantsTopFemales as $participantsTopFemale) {
                 $ParticipantsTopFemalesArray[$participantsTopFemale['p_slug']] = array('p_first_name' => $participantsTopFemale['p_first_name'], 'p_last_name' => $participantsTopFemale['p_last_name'], 'p_display_name' => $participantsTopFemale['p_display_name'], 'rp_age' => $participantsTopFemale['rp_age'], 'rp_time' => $participantsTopFemale['rp_time']);
             }
-            $raceArray[$race['race_name']] = array('race_id' => $race['race_id'], 'race_slug' => $race['race_slug'], 'participants_top_males' => $participantsTopMalesArray, 'participants_top_females' => $ParticipantsTopFemalesArray);
+            $raceArray[$race['race_name']] = array('race_id' => $race['race_id'], 'rt_slug' => $race['rt_slug'], 'race_slug' => $race['race_slug'], 'participants_top_males' => $participantsTopMalesArray, 'participants_top_females' => $ParticipantsTopFemalesArray);
         }
 
 
