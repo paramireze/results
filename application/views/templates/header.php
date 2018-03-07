841998abb0f291da98b08d119a5607fdb6eb7cf2
paramireze <paramireze@gmail.com>
2018-03-06 20:35:32 -0600

    Tweaks to head nav bar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -16,6 +16,15 @@
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
         <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
             <ul class="navbar-nav">
+                <?php
+
+                if (!$this->ion_auth->is_admin()) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa d-inline fa-lg fa-bookmark-o"></i> add race</a>
+                    </li>
+                <?php
+                }
+                ?>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="fa d-inline fa-lg fa-bookmark-o"></i> Bookmarks</a>
                 </li>
@@ -23,7 +32,7 @@
                     <a class="nav-link" href="#"><i class="fa d-inline fa-lg fa-envelope-o"></i> Contacts</a>
                 </li>
             </ul>
-            <a class="btn navbar-btn ml-2 text-white btn-secondary"><i class="fa d-inline fa-lg fa-user-circle-o"></i> Sign in</a>
+            <a href="<?php echo base_url(); ?>auth/login" class="btn navbar-btn ml-2 text-white btn-secondary"><i class="fa d-inline fa-lg fa-user-circle-o"></i> Sign in</a>
         </div>
     </div>
 </nav>
